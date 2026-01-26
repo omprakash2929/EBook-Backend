a999013d0eba025aa09028f4946ecc2c6f8b52ca
omprakash2929 <om.prakashchauhan2929@gmail.com>
2026-01-26 18:04:19 +0530

    Add User validation

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,24 @@
 import express from 'express'
 
 import globalErrorHandler from './middleware/globalErrorHandler';
+import userRouter from './user/userRouter';
 
 
 const app = express();
 
+app.use(express.json())
 
 app.get('/health', (req,res,next)=>{
     
   
     res.json({message:"Server are Health"})
-})
+});
+
+//* User Routes
+app.use('/api/users',userRouter)
+
+
+
 
 // Gloabal Error Handler
 
